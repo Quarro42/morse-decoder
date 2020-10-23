1686b6bc97405d85d2764e70037390546fe74ec9
Nick Kim <lignum.nlk@gmail.com>
2020-10-23 23:26:25 +0400

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const { strikethrough } = require("colors");
+
 const MORSE_TABLE = {
     '.-':     'a',
     '-...':   'b',
@@ -39,6 +41,38 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    
+  let decArray = [];
+  let morseArray = [];
+  let resultStr = '';
+
+  for (let i = 0; i < expr.length; i = i + 10) {
+    let currentArrElem = expr.slice(i, i + 10);
+    decArray.push(currentArrElem)
+  }
+
+  for (let j = 0; j < decArray.length; j++) {
+    let currentArrElem = decArray[j].slice(decArray[j].indexOf(1));
+    let morseSymbol = '';
+    for (let m = 0; m < currentArrElem.length; m = m + 2) {
+      let encodedSymbol = currentArrElem.slice(m, m + 2);
+      if (encodedSymbol === '10') {
+        encodedSymbol = '.';
+        morseSymbol += encodedSymbol;
+      } else if (encodedSymbol === '11') {
+        encodedSymbol = '-';
+        morseSymbol += encodedSymbol;
+      } else {
+        morseArray.push(' ');
+      }
+    }
+    morseSymbol && morseArray.push(morseSymbol);
+  }
+
+  for (let i = 0; i < morseArray.length; i++) {
+resultStr = morseArray[i] !== ' ' ? resultStr + MORSE_TABLE[morseArray[i]] : resultStr + ' '; 
+ }
+  return resultStr;
 }
 
 module.exports = {
